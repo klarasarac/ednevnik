1426ff67e8513f7adcd51276359ccfe014329a97
klarasarac <klarasarac55@gmail.com>
2024-01-24 14:26:07 +0100

    first commit

---
+++ src/main/java/ba/sum/fsre/ednevnik/config/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,26 @@
+package ba.sum.fsre.ednevnik.config;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        var authorities = authentication.getAuthorities();
+
+        if (authorities.stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))) {
+            response.sendRedirect("/users");
+        } else if (authorities.stream().anyMatch(a -> a.getAuthority().equals("TEACHER"))) {
+            response.sendRedirect("/teacher");
+        } else if (authorities.stream().anyMatch(a -> a.getAuthority().equals("STUDENT"))) {
+            response.sendRedirect("/student");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
